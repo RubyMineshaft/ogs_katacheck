089638e7e6e2cf077c732bbfaaa2ebf13146af67
mattetress <mattetress@me.com>
2020-10-22 10:32:34 -0500

    add CLI flow

--- lib/ogs_katacheck/cli.rb
+++ lib/ogs_katacheck/cli.rb
@@ -1,1 +1,58 @@
 class OGSKataCheck::CLI
+
+  def call
+    puts "Enter the Game ID you'd like to check for AI use:"
+
+    game_id = gets.strip
+    get_reviews(game_id)
+    show_review_menu(game_id)
+    check_id = gets.strip.to_i
+    check(check_id)
+    puts ""
+    puts "Check another game? (Y/N)"
+    input = gets.strip.downcase
+    if input == "y"
+      Review.destroy_all
+      FullReview.destroy_all
+      call
+    else
+      show_farewell
+    end
+  end
+
+  def get_reviews(game_id)
+    response = Faraday.get "https://online-go.com/api/v1/games/#{game_id}/ai_reviews"
+    data = JSON.parse(response.body)
+    data.each_with_index do |review, index|
+      Review.new(review, number: index + 1)
+    end
+
+  end
+
+  def show_review_menu(game_id)
+    puts ""
+    puts ""
+    puts "Game ID: #{game_id}"
+    puts ""
+    puts "The following reviews are available:"
+
+    Review.all.each do |review|
+      puts "#{review.number}.  #{review.id} - #{review.engine} - #{review.type} - #{review.date}"
+    end
+
+    puts ""
+    puts "Enter the number of a review to check:"
+
+  end
+
+  def check(check_id)
+    puts "Processing. Please wait..."
+    review = Review.find_by_number(check_id)
+    review.check
+  end
+
+  def show_farewell
+    puts "Goodbye!"
+  end
+
+end
